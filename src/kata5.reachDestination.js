98c13f76ac6308ab56c54882e5211f9266e64d48
Ed Adkins <ejmadkins@gmail.com>
2022-02-26 13:19:03 +0000

    add function to return approx time to desitation

--- src/kata5.reachDestination.js
+++ src/kata5.reachDestination.js
@@ -1,3 +1,8 @@
-const reachDestination = (distance, speed) => {};
+const reachDestination = (distance, speed) => {
+  return `I should be there in ${Math.round((distance / speed) * 2) /
+    2} hours.`;
+};
+
+console.log(reachDestination(44, 10));
 
 module.exports = reachDestination;
