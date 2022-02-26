c12825fd86918314038b3026d78663aa59289993
Ed Adkins <ejmadkins@gmail.com>
2022-02-26 11:05:01 +0000

    add function to reverse numbers

--- src/kata3.numberToReversedDigits.js
+++ src/kata3.numberToReversedDigits.js
@@ -1,3 +1,9 @@
-const numberToReversedDigits = number => {};
+const numberToReversedDigits = (number) => {
+  return number
+    .toString()
+    .split("")
+    .reverse()
+    .map((stringNum) => parseInt(stringNum, 0));
+};
 
 module.exports = numberToReversedDigits;
