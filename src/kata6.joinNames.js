91b3115ebd5c81b72a56539cccb38de6e671a83e
Ed Adkins <ejmadkins@gmail.com>
2022-02-26 17:08:20 +0000

    check a string is returned with a series of names

--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -1,3 +1,8 @@
-const joinNames = namesObj => {};
+const joinNames = (namesObj) => {
+  return `${namesObj
+    .map((name) => name.name)
+    .slice(0, -1)
+    .join(", ")} & ${namesObj.slice(-1).pop().name}`;
+};
 
 module.exports = joinNames;
