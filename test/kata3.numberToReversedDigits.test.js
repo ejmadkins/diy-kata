a4a2acb45011ad9c672c2b719295a77dd64648f6
Ed Adkins <ejmadkins@gmail.com>
2022-02-26 11:08:34 +0000

    add tests to check number is reversed and an array is returned

--- test/kata3.numberToReversedDigits.test.js
+++ test/kata3.numberToReversedDigits.test.js
@@ -1,5 +1,10 @@
 const { numberToReversedDigits } = require("../src");
 
 describe("numberToReversedDigits", () => {
-  test("returns a reversed array of the number's digits", () => {});
+  test("checks that an array is returned", () => {
+    expect(Array.isArray(numberToReversedDigits(12345))).toBe(true);
+  });
+  test("returns a reversed array of the number's digits", () => {
+    expect(numberToReversedDigits(12345)).toEqual([5, 4, 3, 2, 1]);
+  });
 });
