f04f6e2f2ee366faa55ed2725ff752bcf9b0dde1
Ed Adkins <ejmadkins@gmail.com>
2022-02-26 12:36:48 +0000

    add function to return human, cat and dog years

--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,3 +1,10 @@
-const humanCatDogYears = number => {};
+const humanCatDogYears = (number) => {
+  if (number <= 2) return number < 2 ? [1, 15, 15] : [2, 24, 24];
+  return [number, 24 + (number - 2) * 4, 24 + (number - 2) * 5];
+};
+
+console.log(humanCatDogYears(10));
+console.log(humanCatDogYears(2));
+console.log(humanCatDogYears(1));
 
 module.exports = humanCatDogYears;
